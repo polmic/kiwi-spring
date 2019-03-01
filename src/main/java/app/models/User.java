8f90c3b33bcfa4f1bf49f82a08c27a57da541c99
Paul <paul.micas@icloud.com>
2019-03-01 22:45:42 +0100

    fix Lists

--- src/main/java/app/models/User.java
+++ src/main/java/app/models/User.java
@@ -1,12 +1,8 @@
 package app.models;
 
 import lombok.Data;
-import org.h2.table.Plan;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import java.math.BigDecimal;
+import javax.persistence.*;
 import java.util.List;
 
 @Entity
@@ -19,24 +15,21 @@ public class User {
   private String name;
   private String email;
   private String password;
-  private List<Plant> plants;
 
   public User() {
   }
 
-  public User(Long id, String name, String email, String password, List<Plant> plants) {
+  public User(Long id, String name, String email, String password) {
     this.id = id;
     this.name = name;
     this.email = email;
     this.password = password;
-    this.plants = plants;
   }
 
-  public User(String name, String email, String password, List<Plant> plants) {
+  public User(String name, String email, String password) {
     this.name = name;
     this.email = email;
     this.password = password;
-    this.plants = plants;
   }
 
 }
--- src/main/java/app/models/UserGroup.java
+++ src/main/java/app/models/UserGroup.java
@@ -14,18 +14,8 @@ public class UserGroup {
   @Id
   @GeneratedValue
   private Long id;
-  private List<Long> userIds;
 
   public UserGroup() {
-  }
-
-  public UserGroup(List<Long> userIds) {
-    this.userIds = userIds;
-  }
-
-  public UserGroup(Long id, List<Long> userIds) {
-    this.id = id;
-    this.userIds = userIds;
   }
 
 }
